718293937a3a69e863f23494dda0e2f51bd3b932
Umut Sağlam <96023206+umutsaglam@users.noreply.github.com>
2024-05-08 06:30:18 +0000

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,58 @@
+#  █████╗ ██████╗ ████████╗    ███████╗██████╗  █████╗ ███╗   ███╗███████╗██╗    ██╗ ██████╗ ██████╗ ██╗  ██╗
+# ██╔══██╗██╔══██╗╚══██╔══╝    ██╔════╝██╔══██╗██╔══██╗████╗ ████║██╔════╝██║    ██║██╔═══██╗██╔══██╗██║ ██╔╝
+# ███████║██████╔╝   ██║       █████╗  ██████╔╝███████║██╔████╔██║█████╗  ██║ █╗ ██║██║   ██║██████╔╝█████╔╝ 
+# ██╔══██║██╔═══╝    ██║       ██╔══╝  ██╔══██╗██╔══██║██║╚██╔╝██║██╔══╝  ██║███╗██║██║   ██║██╔══██╗██╔═██╗ 
+# ██║  ██║██║        ██║       ██║     ██║  ██║██║  ██║██║ ╚═╝ ██║███████╗╚███╔███╔╝╚██████╔╝██║  ██║██║  ██╗
+# ╚═╝  ╚═╝╚═╝        ╚═╝       ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝
+#
+#
+#                            by umutsaglam
+#
+
+
+clear
+
+sudo chmod +x /etc/
+
+clear
+
+sudo chmod +x /usr/share/doc
+
+clear
+
+sudo rm -rf /usr/share/doc/APT-Framework/
+
+clear
+
+cd /etc/
+
+clear
+
+sudo rm -rf /etc/APT-Framework
+
+clear
+
+mkdir APT-Framework
+
+clear
+
+cd APT-Framework
+
+clear
+
+git clone https://github.com/umutsaglam/APT-Framework.git
+
+clear
+
+cd APT-Framework
+
+clear
+
+sudo chmod +x install.sh
+
+clear
+
+./install.sh
+
+clear
+
