77b906a3df5d9c9a18fe851270564ffca7245e7e
Umut Sağlam <96023206+umutsaglam@users.noreply.github.com>
2024-05-08 06:34:25 +0000

    Add files via upload

---
+++ uninstall.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import shutil
+
+if os.path.exists('/data/data/com.termux/files'):
+    pass
+else:
+    if os.geteuid() != 0: 
+        exit("Bu betiği çalıştırmak için kök ayrıcalıklarına sahip olmalısınız.")
+        sys.exit()
+
+inp = input('APT-Framework\'i Kaldırmak İstediğinizden Emin Misiniz? (evet/hayır) = ')
+if inp.lower() == 'evet' or inp.lower() == 'e':
+    if os.path.exists('/data/data/com.termux/files'):
+        INSTALL_DIR = "$PREFIX/usr/share/doc/APT-Framework"
+        BIN_DIR = "$PREFIX/usr/bin/"
+    else:
+        INSTALL_DIR = "/usr/share/doc/APT-Framework"
+        BIN_DIR = "/usr/bin/"
+    
+    print(BIN_DIR + 'APT-Framework' + ' Dosyası Siliniyor')
+    try:
+        os.remove(BIN_DIR + 'APT-Framework')
+    except:
+        print('Uygulama kaldırılamadı, uygun izinlere sahip olduğunuzdan veya yeniden yükleyip kaldırmak istediğinizden emin olun')
+        sys.exit()
+
+    print(INSTALL_DIR + ' Klasörü Siliniyor')
+    try:
+        shutil.rmtree(INSTALL_DIR)
+    except:
+        print('Uygulama kaldırılamadı, uygun izinlere sahip olduğunuzdan veya yeniden yükleyip kaldırmak istediğinizden emin olun')
+        sys.exit()
+    
+    print('Kaldırma Başarılı!')
+
+else:
+    print('Kaldırma İptal Edildi!')
